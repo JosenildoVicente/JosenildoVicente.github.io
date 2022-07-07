8abb510df047cd62ce34f3e5e4de83f51540bd8c
JosenildoVicente <josenildovr@gmail.com>
2022-07-06 23:52:58 -0300

    Begin of main page

--- js/script.js
+++ js/script.js
@@ -1,43 +1,16 @@
-class MobileNavbar{
-    constructor(mobileMenu, navList, navLinks){
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
+const projs = document.getElementById("projects-list");
+const proj = document.querySelectorAll("#projects-list div");
 
-        this.handleClick = this.handleClick.bind(this);
+let idx = 0;
+
+function carrossel(){
+    idx++;
+
+    if(idx > proj.length -1){
+        idx=0;
     }
 
-    animateLinks(){
-        this.navLinks.forEach((link, index) => {
-            link.style.animation
-                ? (link.style.animation = "")
-                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index/24 +0.3}s`);
-        })
-    }
-
-    handleClick(){
-        this.navList.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass);
-        this.animateLinks();
-    }
-
-    addClickEvent(){
-        this.mobileMenu.addEventListener("click",this.handleClick);
-    }
-
-    init(){
-        if (this.mobileMenu){
-            this.addClickEvent();
-        }
-        return this;
-    }
+    projs.style.transform = `translateX(${-idx * 500}px)`;
 }
 
-const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".li-menu",
-);
-
-mobileNavbar.init();
+// setInterval(carrossel,1800);
