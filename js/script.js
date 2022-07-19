6125fe96b6388b4e36ea0e20f741d624b02c1f40
JosenildoVicente <josenildovr@gmail.com>
2022-07-19 15:32:11 -0300

    Initial Responsive done

--- js/script.js
+++ js/script.js
@@ -1,21 +1,14 @@
 const btn = document.getElementById('button-projects');
 const block = document.getElementById("block-2");
-const proj = document.getElementById("projects");
-const list = document.getElementById("projects-list");
 
 btn.addEventListener('click', function handleClick() {
     const initialText = 'Ver mais';
 
     if (btn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
         btn.textContent = 'Ver menos';
-        block.style.visibility = 'visible';
-        proj.style.height = '1000px';
-        list.style.height = '800px';
-
+        block.style.display = 'flex';
     } else {
         btn.textContent = initialText;
-        block.style.visibility = 'hidden';
-        proj.style.height = '600px';
-        list.style.height = '400px';
+        block.style.display = 'none';
     }
 });
